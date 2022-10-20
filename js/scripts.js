438a548b3ed7527f459ee6b21e65c82a2c6b6371
Christina Tim <ChristinaDesigns@outlook.com>
2022-10-20 16:32:42 +0300

    Shop page

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+// Pagination
+const presPages = document.getElementsByClassName("shop-presentation");
+let currentPage = 0;
+
+// console.log(presPages);
+
+presPages[currentPage].style.display = "initial";
+
+function showPage(data) {
+    let number;
+
+    switch(data){
+        case "next":
+            number = currentPage + 2;
+            break;
+        case "prev":
+            number = currentPage;
+            break;
+        default:
+            number = data;
+            break;
+    }
+
+    // if(presPages.length < number || number < 1){
+    //     return;
+    // }
+    
+    let prevLink  = document.getElementById(`page-link-${currentPage + 1}`);
+    let currentLink = document.getElementById(`page-link-${number}`);
+    
+    if(number > 1){
+        document.getElementById('page-link-prev').classList.remove('disabled');
+    }
+    else if(number == 1){
+        document.getElementById('page-link-prev').classList.add('disabled');
+    }
+    if(number >= presPages.length - 1){
+        document.getElementById('page-link-next').classList.add('disabled');
+    }
+    if(number < presPages.length){
+        document.getElementById('page-link-next').classList.remove('disabled');
+    }
+
+    prevLink.classList.remove("active");
+    currentLink.classList.add("active");
+
+    
+    presPages[currentPage].style.display = "none";
+    presPages[number-1].style.display = "initial";
+    currentPage = number - 1;
+    
+
+}
