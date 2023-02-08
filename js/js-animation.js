9594b00d39e6c2238fbaa0ae9837f05917257f7a
Christina Tim <ChristinaDesigns@outlook.com>
2023-02-08 14:12:02 +0200

    Update js-animation.js

--- js/js-animation.js
+++ js/js-animation.js
@@ -31,3 +31,9 @@ function reveal(){
 window.addEventListener("scroll", reveal);
 
 reveal();
+
+// INTRO About me
+const tl = gsap.timeline({defaults: {ease: 'power1.out' } });
+
+tl.to(".text", {y: " 0%", duration: 1, stager: 0.25 });
+tl.to(".slider", { y: "100%", duration: 1.5 } );
